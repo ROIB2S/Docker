e295da3e7ebad15d431371a28f2e73ba7cc480da
Grant Moyle <grant.a.moyle@roitraining.com>
2022-12-09 11:53:00 -0500

    Updated

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+
 from waitress import serve
 import os
 import logging
@@ -13,9 +15,21 @@ def hello():
   randomnum = random.randint(1, 100000000)/100
   return "Your Random Number is " + str(randomnum) + "!\n"
 
+@app.route("/demo",methods=['GET'])
+def demoget():
+  returnstring = 'demo - GET received'
+  returnstring = "SKU " + str(request.args.get('sku')) + " Recevied"
+  return returnstring
+
+@app.route("/demo",methods=['POST'])
+def demopost():
+  returnstring = "demo - POST received\n"
+  returnstring = "SKU " + str(request.args.get('sku')) + " Recevied"
+  return returnstring
+
 @app.route("/version")
 def version():
-  return "ROIGCP Demo 1.0\n"
+  return "ROIB2S Demo 1.0\n"
 
 if __name__ == "__main__":
   serve(app,host="0.0.0.0",port=int(os.environ.get("PORT", 8080)))
